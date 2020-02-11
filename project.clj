384f7d0b6143d3a4b20de266b7260b184dd3cdc5
mmeixner <michaelmeixner@me.com>
2020-02-11 13:18:50 +0100

    Add project.clj

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject salinas "0.0.1-alpha"
+  :description "A library dealing with musical pitches."
+  :url "https://github.com/mmeixner/salinas"
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.10.1"]])
